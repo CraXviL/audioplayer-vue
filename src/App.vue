822730a34b69d87922795021bb9e56a1ca830472
CraXviL <strebkov87@yandex.ru>
2019-03-28 17:49:11 +0300

    first build

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,46 @@
 <template>
 	<b-container>
 		<b-row class="my-3">
-		  <b-col md="3" cols="6" class="d-flex justify-content-between align-items-center order-1">
-		  	<img src="/img/back.svg" alt="back.svg">
-		  	<img v-if="isPlaying"
+		  <b-col md="3" cols="6" 
+		  				class="d-flex justify-content-between align-items-center order-1"
+		  				>
+		  	<img src="/img/back.svg" alt="back.svg" @click="playPrevTrack">
+		  	<img v-if="!isPlaying"
 		  				src="/img/play.svg"
 		  				alt="play.svg"
-		  				@click="setPlaying">
-		  	<img v-else="!isPlaying"
+		  				@click="changePlayerState">
+		  	<img v-else="isPlaying"
 		  				src="/img/pause.svg"
 		  				alt="pause.svg"
-		  				@click="setPlaying">
-		  	<img src="/img/next.svg" alt="next.svg">
+		  				@click="changePlayerState">
+		  	<img src="/img/next.svg" alt="next.svg" @click="playNextTrack">
 		  </b-col>
-		  <b-col md="6" cols="12" class="d-flex align-items-center order-sm-2 order-3">
+		  <b-col md="6" cols="12"
+		  				class="d-flex flex-column align-items-center order-md-2 order-3 p-0"
+						  @click="onClickProgress"
+						  >
+				<h6>{{ currentTrackName }} {{ currentProgressInMinutes }}</h6>
 		  	<b-progress id="progress"
 		  							class="w-100"
-		  							:value="progress"
+		  							:value="currentProgressInPixels"
 		  							:max="maxProgress"
 		  							></b-progress>
 		  </b-col>
-		  <b-col md="3" cols="6" class="d-flex align-items-center order-sm-3 order-2">
+		  <b-col md="3" cols="6" 
+		  				class="d-flex align-items-center justify-content-center order-md-3 order-2"
+		  				@click="onClickVolume"
+		  				>
 		  	<img v-if="!isMuted" 
 		  				src="/img/volume.svg"
 		  				alt="volume.svg"
-		  				@click="setMute"
+		  				@click="onClickMute"
 		  				>
 		  	<img v-else="isMuted"
 		  				src="/img/mute.svg"
 		  				alt="mute.svg"
-		  				@click="setMute"
+		  				@click="onClickMute"
 		  				>
 		  	<b-progress id="volume"
-		  							class="w-100"
 		  							:value="volume"
 		  							:max="100"
 		  							></b-progress>
@@ -45,7 +53,7 @@
 		</b-row>
 		<b-row>
 			<b-col>
-				<b-table hover thead-class="d-none" :items="visibleTracks" @row-clicked="setTrack"/>
+				<b-table hover thead-class="d-none" :items="visibleTracks" :fields="fields" @row-clicked="onClickList"/>
 			</b-col>
 		</b-row>
 	</b-container>
@@ -53,18 +61,90 @@
 
 <script>
 
-	import {mapGetters, mapMutations} from 'vuex';
+	import {mapGetters, mapMutations, mapActions} from 'vuex';
 
   export default {
-  	computed: mapGetters(['progress', 'maxProgress', 'isPlaying', 'isMuted', 'volume', 'visibleTracks']),
+  	computed: mapGetters(['currentTrackId', 'currentProgress', 'currentProgressInPixels', 'currentProgressInMinutes', 'trackDuration', 'maxProgress', 'isPlaying', 'isMuted', 'volume', 'visibleTracks', 'fields', 'currentTrackPath', 'currentTrackName']),
   	methods: {
-  		...mapMutations(['setPlaying', 'setMute']),
-  		findTrack(value) {
-  			this.$store.commit('findTrack', {value});
+  		...mapMutations(['setMute', 'findTrack']),
+  		...mapActions(['playPrevTrack', 'playNextTrack']),
+  		changePlayerState() {
+  			if (this.isPlaying) {
+  				this.audioPlayer.pause();
+  				this.$store.commit('changePlayerState', false);
+  			} else {
+  				this.audioPlayer.play();
+  				this.$store.commit('changePlayerState', true);
+  			}
   		},
-  		setTrack(e, index, item) {
-  			$(item.target).parents('tr').addClass('bg-info').siblings().removeClass('bg-info');
-  			this.$store.commit('setTrack', {index});
+			onClickProgress(e) {
+				if (e.target.className === "progress-bar" || e.target.id === 'progress') {
+					let pixelsInSeconds = this.maxProgress / this.trackDuration;
+					let progressInSeconds = e.layerX / pixelsInSeconds
+					this.$store.dispatch('calcCurrentProgress', progressInSeconds);
+					this.audioPlayer.currentTime = progressInSeconds;
+				}
+			},
+  		onClickVolume(e) {
+  			if (e.target.className === "progress-bar" || e.target.id === 'volume') {
+	  			this.$store.commit('setVolume', e);
+	  			this.audioPlayer.volume = this.volume/100;
+	  		}
+  		},
+  		onClickMute(e) {
+  			if (this.isMuted) {
+  				this.audioPlayer.muted = false;
+  				this.$store.commit('setMute', false);
+  			} else {
+  				this.audioPlayer.muted = true;
+  				this.$store.commit('setMute', true);
+  			}
+  		},
+  		onClickList(e, index, item) {
+  			if (this.visibleTracks[index]._rowVariant === 'active') this.changePlayerState();
+  			else this.$store.commit('setCurrentTrack', index);
+  		}
+  	},
+  	mounted() {
+			audioPlayerInit: {
+				this.audioPlayer = new Audio();
+			  this.audioPlayer.volume = this.volume/100;
+				this.audioPlayer.src = this.currentTrackPath;
+				console.log('audioPlayerInit: ', this.audioPlayer);
+				this.$store.commit('listInit');
+				$(this.audioPlayer).one('loadedmetadata', () => {
+					console.log(this.audioPlayer, this.audioPlayer.duration);
+					this.$store.commit('setTrackDuration', this.audioPlayer.duration);
+				});
+			}
+			addIdsToTrs: {
+				for (var i = 0; i <= this.visibleTracks.length - 1; i++) {
+					$('tbody tr:nth-child(' + (i+1) + ')').attr('id', 'tr_' + i);
+				}
+			}
+  	},
+  	watch: {
+  		isPlaying() {
+				if (this.isPlaying) {
+					window.tickId = window.setInterval(() => {
+						if (this.currentProgress === this.trackDuration) {
+							this.$store.dispatch('playNextTrack');
+						}
+						this.$store.dispatch('calcCurrentProgress', this.audioPlayer.currentTime);
+					}, 1000);
+				} else {
+					window.clearInterval(window.tickId);
+				}
+  		},
+  		currentTrackPath() {
+				this.audioPlayer.pause();
+				this.audioPlayer.src = this.currentTrackPath;
+				$(this.audioPlayer).one('loadeddata', () => this.audioPlayer.play());
+				$(this.audioPlayer).one('loadedmetadata', () => {
+					console.log(this.audioPlayer, this.audioPlayer.duration);
+					this.$store.commit('setTrackDuration', this.audioPlayer.duration);
+				});
+				this.$store.commit('changePlayerState', true);
   		}
   	}
   };
--- src/store.js
+++ src/store.js
@@ -1,3 +1,4 @@
+/* global process */
 import Vue from 'vue';
 import Vuex from 'vuex';
 
@@ -7,59 +8,115 @@ export default new Vuex.Store({
 
 	state: {
 		tracks: [
-      { name: 'valve', artist: 'audiophyla', duration: '6:15' },
-      { name: 'Sunny9', artist: 'bela.bar', duration: '4:17' },
-      { name: 'Y=MX+B', artist: 'Peppy & The Firing Squad', duration: '3:22' },
-      { name: '2D', artist: 'Seed A.I.', duration: '5:33' }
+      { id: 0, name: 'Sunny9', artist: 'bela.bar', duration: '4 : 17' },
+      { id: 1, name: 'Y=MX+B', artist: 'Peppy & The Firing Squad', duration: '3 : 22' },
+      { id: 2, name: '2D', artist: 'Seed A.I.', duration: '5 : 33' }
     ],
+    fields: ['name', 'artist', 'duration'],
     visibleTracks: [
-      { name: 'valve', artist: 'audiophyla', duration: '6:15' },
-      { name: 'Sunny9', artist: 'bela.bar', duration: '4:17' },
-      { name: 'Y=MX+B', artist: 'Peppy & The Firing Squad', duration: '3:22' },
-      { name: '2D', artist: 'Seed A.I.', duration: '5:33' }
+      { id: 0, name: 'Sunny9', artist: 'bela.bar', duration: '4 : 17' },
+      { id: 1, name: 'Y=MX+B', artist: 'Peppy & The Firing Squad', duration: '3 : 22' },
+      { id: 2, name: '2D', artist: 'Seed A.I.', duration: '5 : 33' }
     ],
-    progress: 10,
+    currentTrackId: 0,
+    currentProgress: 0,
+    trackDuration: 0,
     maxProgress: 100,
     volume: 50,
 		isPlaying: false,
 		isMuted: false
 	},
 	getters: {
-		isPlaying(state){
-			return state.isPlaying;
+		currentTrackId: state => state.currentTrackId,
+		fields: state => state.fields,
+		currentTrackPath: state => './tracks/' +
+			state.tracks[state.currentTrackId].artist + ' - ' +
+			state.tracks[state.currentTrackId].name + '.mp3',
+		currentTrackName: state => state.tracks[state.currentTrackId].artist +
+			' - ' + state.tracks[state.currentTrackId].name,
+		currentProgress: state => state.currentProgress,
+		currentProgressInPixels: (state, getters) => {
+			let pixelsInSeconds = getters.maxProgress/getters.trackDuration;
+			return parseInt(getters.currentProgress * pixelsInSeconds);
 		},
-		isMuted(state){
-			return state.isMuted;
+		currentProgressInMinutes: (state, getters) => {
+				let minutes = parseInt(getters.currentProgress / 60);
+				let seconds;
+				minutes < 1 ? seconds = parseInt(getters.currentProgress) :
+					seconds = parseInt(getters.currentProgress % (minutes*60));
+				return minutes + ' : ' + seconds;
 		},
-		visibleTracks(state){
-			return state.visibleTracks;
-		},
-		progress(state){
-			return state.progress;
-		},
-		maxProgress(state){
-			return state.maxProgress;
-		},
-		volume(state){
-			return state.volume;
-		}
+		trackDuration: state => state.trackDuration,
+		isPlaying: state => state.isPlaying,
+		isMuted: state => state.isMuted,
+		visibleTracks: state => state.visibleTracks,
+		progress: state => state.progress,
+		maxProgress: state => state.maxProgress,
+		volume: state => state.volume
 	},
 	mutations: {
-		setPlaying(state){
-			state.isPlaying = !state.isPlaying;
+		changePlayerState(state, condition){
+			state.isPlaying = condition;
 		},
-		setMute(state){
-			state.isMuted = !state.isMuted;
+		setMute(state, condition){
+			state.isMuted = condition;
 		},
-		findTrack(state, data){
-			if (data.value !== '') {
-				state.visibleTracks = []
-			} else {
-				state.visibleTracks = state.tracks
+		setTrackDuration(state, duration){
+			state.trackDuration = parseInt(duration);
+		},
+		setVolume(state, e){
+			state.volume = e.layerX;
+		},
+		listInit(state){
+			state.maxProgress = $('#progress').width();
+			state.visibleTracks[0]._rowVariant = 'active';
+		},
+		findTrack(state, value){
+			state.visibleTracks = state.tracks.filter(item => 
+				item.name.toLowerCase().indexOf(value.toLowerCase()) !== -1
+			);
+			state.visibleTracks.forEach(item => {
+				item.id === state.currentTrackId ? item._rowVariant = 'active' : false
+			});
+		},
+		setCurrentTrack(state, index){
+			for (var i = state.visibleTracks.length - 1; i >= 0; i--) {
+				state.visibleTracks[i]._rowVariant = '';
+				state.visibleTracks[i].duration = state.tracks[i].duration;
 			}
+			state.visibleTracks[index]._rowVariant = 'active';
+			state.currentTrackId = state.visibleTracks[index].id;
 		},
-		setTrack(state, data){
-			console.log(data.index);
+		setCurrentProgress(state, progress){
+			state.currentProgress = progress;
+		},
+		setMaxProgress(state, maxProgress){
+			state.maxProgress = maxProgress;
+		},
+		setCurrentProgressInMinutes(state, progress){
+			let track = state.visibleTracks.find(item => item.id === state.currentTrackId);
+			if (track) track.duration = progress;
+		}
+	},
+	actions: {
+		calcCurrentProgress(context, progress){
+			context.commit('setMaxProgress', $('#progress').width());
+			context.commit('setCurrentProgress', parseInt(progress));
+			context.commit('setCurrentProgressInMinutes', context.getters.currentProgressInMinutes);
+		},
+		playPrevTrack(context){
+			let nextTrack;
+			context.state.currentTrackId === 0 ?
+				nextTrack = context.state.tracks.length - 1 :
+				nextTrack = context.state.currentTrackId - 1
+			context.commit('setCurrentTrack', nextTrack);
+		},
+		playNextTrack(context){
+			let nextTrack;
+			context.state.currentTrackId === context.state.tracks.length - 1 ?
+				nextTrack = 0 :
+				nextTrack = context.state.currentTrackId + 1
+			context.commit('setCurrentTrack', nextTrack);
 		}
 	},
 	strict: process.env.NODE_ENV !== 'production'
